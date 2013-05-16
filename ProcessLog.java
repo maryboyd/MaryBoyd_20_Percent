7bb6db9996eefa72ebc2b9884066aa4e6c226105
Mary Boyd <maryboydcrosier@westminster.net>
2013-05-16 10:23:03 -0400

    Calculating Longest Run
    null pointer exceptions are a problem...

--- ProcessLog.java
+++ ProcessLog.java
@@ -121,12 +121,17 @@ public class ProcessLog{
     System.out.println("Would you like to see your total distance?");
     if(ask.nextLine().equals("Yes")){
       double totalMiles = calculateTotalMiles(runs);
-          System.out.println("Your total mileage to date: " + totalMiles);
-          return;
+      System.out.println("Your total mileage to date: " + totalMiles + "miles");
+      return;
+    }
+    System.out.println("Would you like to find your longest recorded run?");
+    if(ask.nextLine().equals("Yes")){
+      System.out.println("Your longest run to date is" + findLongestRunEver(runs) + "miles");
+      return;
     }
     System.out.println("Would you like to find your fastest time for a given workout?");
     if(ask.nextLine().equals("Yes")){
-        double matchdist = 0;
+      double matchdist = 0;
       System.out.println("What distance is the workout you're looking for? Put 0 for Intervals.");
       if(ask.nextInt() == 0){
         System.out.println("What interval distance are you running? In meters please.");
@@ -142,31 +147,31 @@ public class ProcessLog{
         System.out.println("Your fastest time for a distance of " + matchdist + " miles is " + val + " minutes");
       }
       ask.close();
-
-        
-    /* See fastest time for a specific distance or workout.
-     * Have I run this workout before?
-     * When was the last time I ran this workout?
-     * What is the longest run I've done?
-     * Compute my total mileage
-     * How many runs have 
-     * */
+      
+      
+      /* See fastest time for a specific distance or workout.
+       * Have I run this workout before?
+       * When was the last time I ran this workout?
+       * What is the longest run I've done?
+       * Compute my total mileage
+       * How many runs have 
+       * */
+    }
   }
-}
   
   public static Object[][] analyzeLog(){
     Scanner scan = new Scanner("fakelog.csv");
-      scan.nextLine();
-      scan.useDelimiter(",");
-      Object[][] data = new Object[31][9];
-      for(int row = 0; row < 31; row++){
-        for(int col = 0; col < 9; col++){
-          if(scan.hasNext()){
+    scan.nextLine();
+    scan.useDelimiter(",");
+    Object[][] data = new Object[31][9];
+    for(int row = 0; row < 31; row++){
+      for(int col = 0; col < 9; col++){
+        if(scan.hasNext()){
           data[row][col] = scan.next();
-          }
         }
       }
-      return data;
+    }
+    return data;
   }
   
   public static double bestWorkout(double matchdist, Object[][] data){
@@ -174,41 +179,51 @@ public class ProcessLog{
     for(int row = 1; row<31; row++){
       if(data[row][1] instanceof Double && data[row][4] instanceof Double){
         if((Double)(data[row][1]) == matchdist){
-        if((Double)(data[row][4]) < fastestT){
-          fastestT = (Double)(data[row][4]);
+          if((Double)(data[row][4]) < fastestT){
+            fastestT = (Double)(data[row][4]);
+          }
         }
       }
-    }
     } 
     return fastestT;
   }
-
+  
   public static double bestINTERVALworkout(int intDist, int nInts, Object[][] data){
     double fastestAvgT =  400000000.1;
     for(int row = 1; row < 31; row++){
       if(data[row][7] instanceof Integer && data[row][8] instanceof Integer && data[row][9] instanceof Integer){
-      if((Integer)(data[row][7]) == nInts && (Integer)(data[row][8]) == intDist){
-        if((Double)(data[row][9]) < fastestAvgT){
-          fastestAvgT = (Double)(data[row][9]);
+        if((Integer)(data[row][7]) == nInts && (Integer)(data[row][8]) == intDist){
+          if((Double)(data[row][9]) < fastestAvgT){
+            fastestAvgT = (Double)(data[row][9]);
+          }
         }
       }
     }
-    }
-      return fastestAvgT;
-    }
+    return fastestAvgT;
+  }
   
   
   
   public static double calculateTotalMiles(Object[][] data){
-        double totalMiles = 0;
+    double totalMiles = 0;
     for(int row = 1; row < 31; row++){
       if(data[row][1] instanceof Double){
-      totalMiles += ((Double)(data[row][1]));
+        totalMiles += ((Double)(data[row][1]));
       }
       if(data[row][7] != null && data[row][8] != null){
-      totalMiles+= ((Double)(data[row][7])*(Double)(data[row][8])/1600.0);
+        totalMiles+= ((Double)(data[row][7])*(Double)(data[row][8])/1600.0);
       }
     }
     return totalMiles;
+  }
+  
+  public static double findLongestRunEver(Object[][] data){
+    double longestRun = (Double)(data[1][1]);
+    for(int row = 2; row < 31; row++){
+      if(data[row][1] instanceof Double && (Double)data[row][1] > longestRun){
+        longestRun = (Double)data[row][1];
+      }
+    }
+    return longestRun;
   }
 }
